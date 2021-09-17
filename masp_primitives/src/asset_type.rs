6b3bd3c3b6e329b6e9ca6c2fdfc9b68e45d073eb
Alberto Centelles <centelles.alberto@gmail.com>
2021-09-17 19:22:23 +0100

    Revert "i32 nonce"
    This reverts commit 410e7023254e4e8a64962e4d48e01c33f77dabdd.

--- masp_primitives/src/asset_type.rs
+++ masp_primitives/src/asset_type.rs
@@ -9,7 +9,7 @@ use group::{cofactor::CofactorGroup, Group, GroupEncoding};
 #[derive(Debug)]
 pub struct AssetType {
     identifier: [u8; ASSET_IDENTIFIER_LENGTH], //32 byte asset type preimage
-    nonce: Option<i32>,
+    nonce: Option<u8>,
 }
 
 // Abstract type representing an asset
@@ -17,7 +17,7 @@ impl AssetType {
     /// Create a new AsstType from a unique asset name
     /// Not constant-time, uses rejection sampling
     pub fn new(name: &[u8]) -> Result<AssetType, ()> {
-        let mut nonce = 0;
+        let mut nonce = 0u8;
         loop {
             if let Some(asset_type) = AssetType::new_with_nonce(name, nonce) {
                 return Ok(asset_type);
@@ -28,7 +28,9 @@ impl AssetType {
 
     /// Attempt to create a new AssetType from a unique asset name and fixed nonce
     /// Not yet constant-time; assume not-constant-time
-    pub fn new_with_nonce(name: &[u8], nonce: i32) -> Option<AssetType> {
+    pub fn new_with_nonce(name: &[u8], nonce: u8) -> Option<AssetType> {
+        use std::slice::from_ref;
+
         // Check the personalization is acceptable length
         assert_eq!(ASSET_IDENTIFIER_PERSONALIZATION.len(), 8);
 
@@ -39,7 +41,7 @@ impl AssetType {
             .to_state()
             .update(GH_FIRST_BLOCK)
             .update(&name)
-            .update(&nonce.to_le_bytes())
+            .update(from_ref(&nonce))
             .finalize();
 
         // If the hash state is a valid asset identifier, use it
@@ -133,7 +135,7 @@ impl AssetType {
         }
     }
 
-    pub fn get_nonce(&self) -> Option<i32> {
+    pub fn get_nonce(&self) -> Option<u8> {
         return self.nonce;
     }
 }
