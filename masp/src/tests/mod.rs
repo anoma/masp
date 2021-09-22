d4c15c093e44e8dc41da652dc761b7c4de731e80
Georgios Gkitsas <ggkitsas@yahoo.com>
2021-09-22 18:16:31 +0100

    int type fix

--- masp/src/tests/mod.rs
+++ masp/src/tests/mod.rs
@@ -86,13 +86,13 @@ fn sapling_generators() {
 
     let asset_name: [c_uchar; 33] = *b"libmasp_new_asset_identifier test";
     let mut identifier_result: [c_uchar; ASSET_IDENTIFIER_LENGTH] = [0u8; ASSET_IDENTIFIER_LENGTH];
-    let mut nonce_result = 0i32;
+    let mut nonce_result = 0u8;
 
     assert!(libmasp_new_asset_identifier(
         asset_name.as_ptr(),
         33,
         &mut identifier_result,
-        &mut nonce_result as *mut i32
+        &mut nonce_result as *mut u8
     ));
     assert_eq!(
         identifier_result,
@@ -109,7 +109,7 @@ fn sapling_generators() {
     assert!(libmasp_asset_from_name_and_nonce(
         asset_name.as_ptr(),
         33,
-        nonce_result as i32,
+        nonce_result as u8,
         &mut identifier_result_2
     ));
 
