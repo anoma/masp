20a025caa9cad206aa54fc0cdfc203074823337e
joe bebel <55120843+joebebel@users.noreply.github.com>
2023-05-31 02:51:56 -0700

    move Rseed into note.rs

--- masp_primitives/src/sapling.rs
+++ masp_primitives/src/sapling.rs
@@ -494,17 +494,7 @@ impl BorshDeserialize for PaymentAddress {
     }
 }
 
-/// Enum for note randomness before and after [ZIP 212](https://zips.z.cash/zip-0212).
-///
-/// Before ZIP 212, the note commitment trapdoor `rcm` must be a scalar value.
-/// After ZIP 212, the note randomness `rseed` is a 32-byte sequence, used to derive
-/// both the note commitment trapdoor `rcm` and the ephemeral private key `esk`.
-#[derive(Copy, Clone, Debug)]
-pub enum Rseed {
-    BeforeZip212(jubjub::Fr),
-    AfterZip212([u8; 32]),
-}
-
+pub use crate::sapling::note::Rseed;
 pub use crate::sapling::note::nullifier::Nullifier;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
