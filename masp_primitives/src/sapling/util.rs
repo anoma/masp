f82c7b9bc5475f9b3051393f05364f67117c1aec
Murisi Tarusenga <murisit@gmail.com>
2026-04-20 09:12:43 +0200

    Use Keccak hashes in the computation of the Schnorr signature.

--- masp_primitives/src/sapling/util.rs
+++ masp_primitives/src/sapling/util.rs
@@ -1,4 +1,5 @@
-use blake2b_simd::Params;
+use sha3::Keccak256;
+use sha3::Digest;
 
 use crate::consensus::{self, BlockHeight, NetworkUpgrade};
 
@@ -7,11 +8,24 @@ use ff::Field;
 use rand_core::{CryptoRng, RngCore};
 
 pub fn hash_to_scalar(persona: &[u8], a: &[u8], b: &[u8]) -> jubjub::Fr {
-    let mut hasher = Params::new().hash_length(64).personal(persona).to_state();
-    hasher.update(a);
-    hasher.update(b);
-    let ret = hasher.finalize();
-    jubjub::Fr::from_bytes_wide(ret.as_array())
+    // The 64 byte hash that we are computing
+    let mut hash = [0u8; 64];
+    // Compute the first half of the hash
+    let mut hasher0 = Keccak256::new_with_prefix(persona);
+    // The first half's subdomain
+    hasher0.update(&[0]);
+    hasher0.update(a);
+    hasher0.update(b);
+    hasher0.finalize_into((&mut hash[0..32]).try_into().unwrap());
+    // Compute the second half of the hash
+    let mut hasher1 = Keccak256::new_with_prefix(persona);
+    // The second half's subdomain
+    hasher1.update(&[1]);
+    hasher1.update(a);
+    hasher1.update(b);
+    hasher1.finalize_into((&mut hash[32..64]).try_into().unwrap());
+    // Turn the combined hash into a scalar
+    jubjub::Fr::from_bytes_wide(&hash)
 }
 
 pub fn generate_random_rseed<P: consensus::Parameters, R: RngCore + CryptoRng>(
